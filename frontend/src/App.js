a1c178de54d99e45aa9452e94c19e017f485947c
Bavan <70379421+bavan172@users.noreply.github.com>
2026-05-11 21:43:15 -0400

    feat: implement React integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [instances, setInstances] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://127.0.0.1:5000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setInstances(data);
+      });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <h1>CloudCostIQ</h1>
+
+      {instances.map((instance) => (
+
+        <div key={instance.InstanceId}>
+
+          <h2>{instance.Name}</h2>
+
+          <p>{instance.InstanceType}</p>
+
+          <p>{instance.State}</p>
+
+          <p>
+            SSH Open:
+            {instance.SSHOpenToPublic ? " Yes" : " No"}
+          </p>
+
+        </div>
+
+      ))}
+
     </div>
   );
 }
