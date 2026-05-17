bee614ff67cf273d844c4b753c9a34eb8372f307
Bavan <70379421+bavan172@users.noreply.github.com>
2026-05-17 18:25:49 -0400

    chore: register new blueprints and update requirements

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify
 from routes.ec2_routes import ec2_bp
+from routes.s3_routes import s3_bp
+from routes.rds_routes import rds_bp
+from routes.network_routes import network_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
+
 CORS(app)
-app.register_blueprint(ec2_bp, url_prefix="/api/ec2")
+
+app.register_blueprint(ec2_bp, url_prefix = "/api/ec2")
+app.register_blueprint(s3_bp, url_prefix = "/api/s3")
+app.register_blueprint(rds_bp, url_prefix = "/api/rds")
+app.register_blueprint(network_bp, url_prefix = "/api/network")
 
 if __name__ == "__main__":
     app.run(debug=True)
